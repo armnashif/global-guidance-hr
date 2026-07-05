20d3631cfb01b3083a4ee22b96e9b7394a6bf193
Nashif Razzak <Nashif@Nashifs-MacBook-Air-2.local>
2026-07-06 02:02:16 +0530

    feat: simplify HR hubs and add installable mobile app

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'gg-os-shell-v1';
+const SHELL = ['/dashboard', '/command-portal.html', '/manifest.webmanifest', '/static/style.css', '/static/gg-os-192.png', '/static/gg-os-512.png'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match('/command-portal.html'))));
+});
